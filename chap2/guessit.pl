4567f3607cf752e6a1bf2c60c842e7cf5676ad54
tomcha <tomcha@tomcha.net>
2023-06-05 11:01:16 +0900

    get_available_actionsを実装

--- chap2/guessit.pl
+++ chap2/guessit.pl
@@ -2,17 +2,21 @@
 use strict;
 use warnings;
 use List::Util qw(shuffle);
+use Data::Dumper;
 
 sub main(){
-
+#    my $ans = get_available_actions([1, 2, 3], {'kind' => 'ask', 'card' =>5});
+#    print Dumper $ans;
 }
+
+main();
 
 sub deal(){
     my @all_cards = 1..9;
     my @player0_hand;
     my @player1_hand;
     my $rest_card;
-    shuffle(@all_cards);
+    @all_cards = shuffle(@all_cards);
     $rest_card = $all_cards[0];
     for(my $i = 1;$i<9;$i++){
         if ($i % 2 == 0){
@@ -23,3 +27,28 @@ sub deal(){
     }
     return \@player0_hand, \@player1_hand, $rest_card;
 }
+
+sub get_available_actions(){
+    my $now_players_hand = shift;
+    my $prev_action = shift;
+    my @actions;
+    for my $i (1..9){
+        push(@actions, {'kind' => 'ask', 'card' => $i});
+    }
+
+    my @new_actions;
+    if (%$prev_action != {}){
+        my $card_num = $prev_action->{'card'};
+        for my $action (@actions){
+            if ($action->{'card'} != $card_num){
+                push(@new_actions, $action);
+            }
+        }
+        for my $i (1..9){
+            unless (grep{$_ == $i} @$now_players_hand){
+                push(@new_actions, {'kind' => 'guess', 'card' => $i});
+            }
+        }
+    }
+    return \@new_actions;
+};
